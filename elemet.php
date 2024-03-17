321b5f530c1e76e923e197ce7100f34f1c8286d5
python533 <143389231+python533@users.noreply.github.com>
2024-03-17 15:20:20 +0300

    JS -DOM
    JS Document Object Model

---
+++ elemet.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <script type="text/javascript">
+    const imagecol=document.iamges;
+
+    console.log("İmages Collections");
+    for (var i = 0; i < array.length; i++) {
+      console.log(i[]);
+    }
+
+
+    var El1=document.createElement
+    const  El1= document.body.appendChild(document.createElement("src"));
+    const  El1= document.body.appendChild(document.createElement("id"));
+
+    console.log("İmages Collections");
+
+    for (var i = 0; i < array.length; i++) {
+      console.log(i[]);
+    }
+</script>
+</head>
+
+  <body onload="init()">
+  <img src="r1" id="r1">
+  <img src="r2" id="r2">
+  <img src="r3" id="r3">
+  <img src="r4" id="r4">
+ </body>
+ </html>
